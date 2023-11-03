6ae1699037027b473e8eb48e7ea99f3a4cb5d30d
Mostafijur <mostafijur.bd786@gmail.com>
2023-11-03 23:34:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+function App() {
+  const handleadduser=event=>{
+    event.preventDefault();
+    const form=event.target;
+    const name=form.name.value;
+    const email=form.email.value;
+    const user={name,email};
+    console.log(user);
+    fetch('http://localhost:5000/users',{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+    })
+  }
+
+  return (
+    <>
+    <h1>Simple CURD</h1>
+    <form onSubmit={handleadduser} >
+      <input type="text" name="name" id="" />
+      <br />
+      <input type="email" name="email" id="" />
+      <br />
+      <input type="submit" value="Add user" />
+    </form>
+    </>
+  )
+}
+
+export default App
